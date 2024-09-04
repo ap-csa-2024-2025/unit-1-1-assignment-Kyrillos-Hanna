d86ea8cc8169afa219cc93e15aa20984cd729ad6
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-09-04 10:43:28 -0700

    Completed 1.1 assginement

--- problem1.java
+++ problem1.java
@@ -1,1 +1,6 @@
-
+public class problem1 {
+    public static void main(String[] args) {
+        String favoriteSportsPlayer = "                                          Raymond Le                                          ";
+        System.out.println(favoriteSportsPlayer);
+    }
+}
